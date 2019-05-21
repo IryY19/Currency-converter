465bda7df22794d6ddd6d036db78ced76d4bb6c7
Iry Y <iryy1999@gmail.com>
2019-05-21 16:28:06 +0300

    add footer

---
+++ new/footer.php
@@ -0,0 +1,13 @@
+<br>
+<br>
+<footer class="alert alert-dark" role="alert" style=" padding: 15px; opacity: 0.7; bottom: 0; align-content: center;" >
+	<div class="container">
+        <div class="row">
+            <div class="col" align="center">
+                <button onclick="window.location.href='#'" style="width: 50px; height: 50px; border-radius: 10px;" aling="center" type="button" class="btn btn-outline-dark" >Up</button>         
+            </div>
+        </div>
+   
+    </div>
+</footer>
+   
